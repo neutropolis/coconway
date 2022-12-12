510e6cafe1b4063a73477b571211e1c4ec0a8085
Jesús López-González <jesus.lopez.gonzalez+dev@pm.me>
2022-12-12 16:21:09 +0100

    Improves parser to read meta information

--- UI.hs
+++ UI.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, LambdaCase #-}
 
 import Data.Functor
 import Data.List
@@ -19,9 +19,12 @@ import Brick.Widgets.Table
 import Graphics.Vty as V
 
 import Conway
+import Parser
+import Rle
 
 data ConwayState = ConwayState { _generation :: Int
-                               , _board :: Store Cell }
+                               , _board :: Store Cell
+                               , _rle :: Rle }
 makeLenses ''ConwayState
 
 app :: App ConwayState () ()
@@ -36,7 +39,7 @@ instance Show Cell where
   show Live = "▮"
   show Dead = "·"
 
-draw (ConwayState g (Store _ m)) = [center $ help <+> str " " <+> t2 <+> str " " <+> meta] where
+draw (ConwayState g (Store _ m) rle) = [center $ help <+> str " " <+> t2 <+> str " " <+> meta] where
   t = str . intercalate "\n" $ fmap (head . show) <$> toLists m
   t2 = joinBorders $
        withBorderStyle unicode $
@@ -45,10 +48,14 @@ draw (ConwayState g (Store _ m)) = [center $ help <+> str " " <+> t2 <+> str " "
   meta = joinBorders $
          withBorderStyle unicode $
          withAttr mFgAttr $
-         borderWithLabel (str "Meta") $ str $ "generation: " ++ show g
+         borderWithLabel (str "Meta") $
+         str $ "generation: " ++ show g ++ "\n" ++
+               "comment:    " ++ show (fromMaybe "" $ rleComment rle) ++ "\n" ++
+               "name:       " ++ show (fromMaybe "" $ rleName rle)
   help = joinBorders $
          withBorderStyle unicode $
-         borderWithLabel (str "Help") $ str "Press <SPACE> to evolve\nPress <ESC>   to exit"
+         borderWithLabel (str "Help") $
+         str "Press <SPACE> to evolve\nPress <ESC>   to exit"
 
 handleEvent (VtyEvent (V.EvKey (V.KChar ' ') [])) =
   do modify (over generation (+1))
@@ -59,11 +66,20 @@ handleEvent _ = pure ()
 fgAttr = attrName "fgAttr"
 mFgAttr = attrName "mFgAttr"
 
-rle = fromMaybe [] $ parseRle "27b2o$27bobo$29bo4b2o$25b4ob2o2bo2bo$25bo2bo3bobob2o$28bobobobo$29b2obobo$33b2o$19b2o$20bo8bo$20bobo5b2o$21b2o$35bo$36bo$34b3o2b$25bo$25b2o$24bobo4b2o22bo$31bo21b3o$32b3o17bo$34bo17b2o2b$45bo$46b2o12b2o$45b2o14bo$3b2o56bob2o$4bo9b2o37bo5b3o2bo$2bo10bobo37b2o3bo3b2o$2b5o8bo5b2o35b2obo$7bo13bo22b2o15bo$4b3o12bobo21bobo12b3o$3bo15b2o22bo13bo$3bob2o35b2o5bo8b5o$b2o3bo3b2o37bobo10bo$o2b3o5bo37b2o9bo$2obo56b2o$3bo14b2o$3b2o12b2o$19bo2b$11b2o17bo$12bo17b3o$9b3o21bo$9bo22b2o4bobo$38b2o$39bo2b$28b3o$28bo$29bo$42b2o$35b2o5bobo$35bo8bo$44b2o2b$31bo$30bobob2o$30bobobobo$27b2obobo3bo2bo$27bo2bo2b2ob4o$29b2o4bo$35bobo$36b2o!"
+instance Read Cell where
+  readsPrec _ "o" = [(Live, "")]
+  readsPrec _  _  = [(Dead, "")]
 
 main :: IO ()
 main = do
-  let initialState = ConwayState 0 $ Store (1, 1) (matrix 80 100 $ lookups rle)
+  let file = "pattern/gosper-glider-gun.rle"
+  txt <- readFile file 
+  let rle = parseRle txt
+      (x, y) = fromMaybe (0, 0) $ rleOffset rle
+      items = fmap (read . return) <$> rleUncompress rle
+      m = fromLists items
+      matrix = extendTo Dead (nrows m + x) (ncols m + y) $ fromLists items
+      initialState = ConwayState 0 (Store (1, 1) matrix) rle
   finalState <- defaultMain app initialState
   putStrLn "Bye, bye!"
 
